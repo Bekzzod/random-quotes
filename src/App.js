c3d0ce2111251ce559268b6e523b3e70ac97ea07
Bekzod <bek.abakulov@gmail.com>
2019-10-14 17:18:58 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import randomcolor from "randomcolor"
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      quote: "",
+      author: "",
+      color: "",
+      isLoading: true,
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  componentDidMount() {
+    fetch("https://api.quotable.io/random")
+      .then(response => response.json())
+      .then(quote => {
+        this.setState({
+          quote: quote.content,
+          author: quote.author,
+          isLoading: false,
+        })
+      })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.quote !== this.state.quote) {
+      const newColor = randomcolor()
+      this.setState({
+        color: newColor,
+      })
+      document.body.style.backgroundColor = newColor;
+      document.getElementById("button").style.backgroundColor = newColor;
+    }
+  }
+
+  handleClick() {
+    fetch("https://api.quotable.io/random")
+      .then(response => response.json())
+      .then(quote => {
+        this.setState({
+          quote: quote.content,
+          author: quote.author,
+        })
+      })
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return(
+        <div className="container">
+          <img id="loading" src={require('./loading.gif')} alt="loading" />
+        </div>
+      )
+    } else {
+      return (
+        <div style={{color: this.state.color}} className="container">
+          <p>"{this.state.quote}"</p>
+          <p>- {this.state.author}</p>
+          <div id="button" onClick={this.handleClick}>New</div>
+        </div>
+      )
+    }
+  }
 }
 
-export default App;
+export default App
